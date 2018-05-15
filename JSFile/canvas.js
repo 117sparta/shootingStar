c71cfa3f05843dc1fdb76a94a0b750f9a69551c2
jerry <1246286991@qq.com>
2018-05-15 19:44:33 +0800

    finish

--- JSFile/canvas.js
+++ JSFile/canvas.js
@@ -1,27 +1,37 @@
 let drawing = document.getElementById("drawing");
-drawing.width = window.innerWidth;
-drawing.height = window.innerHeight;
+drawing.width = 1366;
+drawing.height = 911;
 console.log(window.innerWidth);
 console.log(window.innerHeight);
 let starGroup = new Array(9);
+let circleGroup = new Array(200);
 for (let i = 0; i < starGroup.length; i++) {
     starGroup[i] = new ShootingStar();
 }
 
+function Circle() {
+    this.x = Math.random() * drawing.width;
+    this.y = Math.random() * drawing.height;
+}
+
+for (let i = 0; i < circleGroup.length; i++) {
+    circleGroup[i] = new Circle();
+}
+
 function ShootingStar() {
-    this.initX = Math.random() * window.innerWidth;
-    this.initY = Math.random() * (window.innerHeight / 8);
+    this.initX = Math.random() * drawing.width;
+    this.initY = Math.random() * (drawing.height / 8) + 50;
     this.x = this.initX;
     this.y = this.initY;
-    this.length = Math.random() * 30 + 14;
-    this.speed = 8;
+    this.length = Math.random() * 30 + 10;
+    this.speed = 4 + Math.random() * 4;
     this.dura = Math.random() * 50 + 20;
     this.reset = function () {
-        this.initX = Math.random() * window.innerWidth;
-        this.initY = Math.random() * (window.innerHeight / 4);
+        this.initX = Math.random() * drawing.width;
+        this.initY = Math.random() * (drawing.height / 4);
         this.x = this.initX;
         this.y = this.initY;
-        this.length = 35;
+        this.length = Math.random() * 30 + 10;
         this.dura = Math.random() * 300 + 20;
     }
 }
@@ -30,41 +40,30 @@ function drawShootingStar() {
     if (drawing.getContext) {
         let context = drawing.getContext("2d");
         context.lineCap = "round";
-        context.fillStyle = "rgba(0,0,0,0.3)";
-        context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+        context.lineWidth = "3";
+        context.fillStyle = "#fff";
         for (let i = 0; i < starGroup.length; i++) {
-            if (starGroup[i].x - starGroup[i].initX >= starGroup[i].dura) {
-                context.clearRect(starGroup[i].x, starGroup[i].y, starGroup[i].length + 2, starGroup[i].length + 2);
+            if (starGroup[i].y - starGroup[i].initY >= starGroup[i].dura) {
+                //context.clearRect(starGroup[i].x, starGroup[i].y, starGroup[i].length + 2, starGroup[i].length + 2);
                 starGroup[i].reset();
                 continue;
             }
-            let step = starGroup[i].length / 5;
-            context.strokeStyle = "rgba(255,255,255,1)";
-            let endX = starGroup[i].x;
-            let endY = starGroup[i].y;
+            context.strokeStyle = "#fff";
             context.beginPath();
             context.moveTo(starGroup[i].x, starGroup[i].y);
-            context.lineTo(starGroup[i].x + starGroup[i].length, starGroup[i].y + starGroup[i].length);
-            /*let opa = 0.3;
-            for (let j = 0; j < 5; j++) {
-                context.beginPath();
-                endX += step;
-                endY += step;
-                context.moveTo(starGroup[i].x + j * step, starGroup[i].y + j * step);
-                context.lineTo(endX, endY);
-                context.stroke();
-                context.closePath();
-                context.strokeStyle = "rgba(255,255,255," + opa + ")";
-                opa += 0.2;
-            }
-            */
+            context.lineTo(starGroup[i].x + starGroup[i].length / 2, starGroup[i].y + starGroup[i].length);
             context.stroke();
             context.closePath();
-            starGroup[i].x += starGroup[i].speed;
+            starGroup[i].x += starGroup[i].speed / 2;
             starGroup[i].y += starGroup[i].speed;
-            //context.clearRect(starGroup[i].x - starGroup[i].speed-2, starGroup[i].y - starGroup[i].speed-2, starGroup[i].speed + 4, starGroup[i].speed + 4);
         }
+        context.save();
+        context.globalCompositeOperation = "destination-out";
+        context.fillStyle = "rgba(0, 0, 0,0.2)";
+        context.fillRect(0, 0, drawing.width, drawing.height);
+        context.restore();
     }
+    requestAnimationFrame(drawShootingStar);
 }
 
-let fallback = setInterval(drawShootingStar, 35);
+requestAnimationFrame(drawShootingStar);
